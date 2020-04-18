990bc65c03d12396618ef4e8a9cebf4d2cb8609c
Scott Wang <scotteau@gmail.com>
2020-04-18 20:19:19 +0800

    Setted default browswer and implemented clock and index

---
+++ src/Clock.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+interface myProps {
+
+}
+
+interface myState {
+
+}
+
+class Clock extends React.Component<myProps, myState> {
+
+    render(): React.ReactNode {
+        return <p>Clock works</p>
+    }
+
+}
+
+export default Clock;
