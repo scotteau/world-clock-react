06d092b68a48f709351c58fcfa217b5eb37184d1
Scott Wang <scotteau@gmail.com>
2020-04-18 20:45:16 +0800

    basic feature done with no style

--- src/Clock.tsx
+++ src/Clock.tsx
@@ -9,9 +9,43 @@ interface myState {
 }
 
 class Clock extends React.Component<myProps, myState> {
+    state = {time: new Date()};
+    timer: any;
+
+    renderTime(){
+        const display = this.state.time.toLocaleTimeString('en-US', {hour12: false}).split(':');
+        return display.map((d, index) => <span key={index}>{d}</span> );
+    }
+
+    renderDate(){
+        const options = {weekday: 'short', month: 'short', day: 'numeric', year: 'numeric' };
+        const date = this.state.time.toLocaleDateString('en-US', options).split(',');
+        return date.map((d,index) => <span key={index}>{d}</span> )
+    }
+
+
 
     render(): React.ReactNode {
-        return <p>Clock works</p>
+        return (
+            <div>
+                {this.renderDate()}
+                {this.renderTime()}
+            </div>
+        )
+    }
+
+    updateTime():void {
+        this.timer = setInterval(() => {
+            this.setState({time: new Date()})
+        }, 1000 );
+    }
+
+    componentDidMount(): void {
+        this.updateTime();
+    }
+
+    componentWillUnmount(): void {
+        clearInterval(this.timer);
     }
 
 }
